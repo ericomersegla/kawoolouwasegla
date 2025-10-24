90582da6f5c84173996a7553beccc8ad9a2d1f2f
KAWO-OLOUWA ERIC-OMER SEGLA <ericomers@gmail.com>
2025-10-23 22:31:00 -0400

    Create index.js

---
+++ src/assets/index.js
@@ -0,0 +1,50 @@
+//import logo from "./images/logo.png"; je masque le logo
+import bannerImg from "./images/mafoto.jpeg"; 
+import photoID from "./images/photoID.jpeg";
+import projectOne from "./images/projects/projectOne.png";
+import projectTwo from "./images/projects/projectTwo.png";
+import projectThree from "./images/projects/projectThree.png";
+import projectFour from "./images/projects/projectFour.png";
+import projectFive from "./images/projects/projectFive.png";
+import projectSix from "./images/projects/projectSix.png";
+import projectSeven from "./images/projects/projectSeven.png";
+import projectEight from "./images/projects/projectEight.jpg";
+import projectNine from "./images/projects/projectNine.webp";
+import projectTen from "./images/projects/ProjectTen.jpg";
+import projectFaceRec from "./images/projects/projetfacerec.jpg";
+import PtojectSummarizer from "./images/projects/PtojectSummarizer.png";
+import ProjectCreditApproval from "./images/projects/ProjectCreditApproval.png";
+import ProjectWebScraping from "./images/projects/ProjectWebScraping.gif";
+import testimonialOne from "./images/testmonial/testimonialOne.jpg";
+import testimonialTwo from "./images/testmonial/testimonialTwo.jpeg";
+import testimonialThree from "./images/testmonial/testimonialThree.jpeg";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+import cv_en from "./reports/cv_en.pdf";
+import cv_fr from "./reports/cv_fr.pdf";
+export {
+ // logo,
+  bannerImg,
+  photoID,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+  projectFive,
+  projectSix,
+  projectSeven,
+  projectEight,
+  projectNine,
+  projectTen,
+  testimonialOne,
+  testimonialTwo,
+  testimonialThree,
+  quote,
+  contactImg,
+  projectFaceRec,
+  PtojectSummarizer,
+  ProjectCreditApproval,
+  ProjectWebScraping,
+  cv_en,
+  cv_fr,
+};
