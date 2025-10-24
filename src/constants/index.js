f09a0335452ce4a83a032eeba2d192e77e9d026b
KAWO-OLOUWA ERIC-OMER SEGLA <ericomers@gmail.com>
2025-10-23 22:53:13 -0400

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+export const navLinksdata = [
+  {
+    _id: 1001,
+    title: "Accueil",
+    link: "home",
+  },
+  /*{
+    _id: 1002,
+    title: "Ma plus value",
+    link: "features",
+  },*/
+  {
+    _id: 1003,
+    title: "Projets",
+    link: "projects",
+  },
+  {
+    _id: 1004,
+    title: "Résumé",
+    link: "resume",
+  },
+
+  {
+    _id: 1006,
+    title: "Contacts",
+    link: "contact",
+  },
+];
